15bd258b267364b75f35fe1174bd742a6d839bd0
Jayanta7865 <jsamanta2003@gmail.com>
2025-08-25 19:50:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let display = document.getElementById("display");
+function appendValue(value){
+    display.value +=value;
+}
+function clearDispay(){
+display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }catch{
+        display.value = "Error";
+    }
+}
